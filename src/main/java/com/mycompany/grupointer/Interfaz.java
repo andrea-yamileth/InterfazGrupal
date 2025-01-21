76a3aa19ee98b0692d4596d955ad070eef72449c
andrea yamileth <andreachaquinga2005@gmail.com>
2025-01-20 22:25:35 -0500

    hice un cambio

--- src/main/java/com/mycompany/grupointer/Interfaz.java
+++ src/main/java/com/mycompany/grupointer/Interfaz.java
@@ -26,21 +26,135 @@ public class Interfaz extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        buttonGroup1 = new javax.swing.ButtonGroup();
+        jLbName = new javax.swing.JLabel();
+        CBxElegir = new javax.swing.JComboBox<>();
+        ChBoxAlm = new javax.swing.JCheckBox();
+        ChBoxTrans = new javax.swing.JCheckBox();
+        ChBoxApo = new javax.swing.JCheckBox();
+        RdBtMañan = new javax.swing.JRadioButton();
+        RdBtTarde = new javax.swing.JRadioButton();
+        BttRegis = new javax.swing.JButton();
+        BttMosReg = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLbName.setFont(new java.awt.Font("PMingLiU-ExtB", 3, 24)); // NOI18N
+        jLbName.setForeground(new java.awt.Color(0, 153, 153));
+        jLbName.setText("Adrian");
+
+        CBxElegir.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        CBxElegir.setForeground(new java.awt.Color(153, 153, 255));
+        CBxElegir.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Estudiante", "Profesional", "Invitado especial" }));
+        CBxElegir.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+
+        ChBoxAlm.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        ChBoxAlm.setText("Almuerzo");
+
+        ChBoxTrans.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        ChBoxTrans.setText("Transporte");
+
+        ChBoxApo.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        ChBoxApo.setText("M.de Apoyo");
+
+        RdBtMañan.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        RdBtMañan.setText("Mañana");
+        RdBtMañan.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                RdBtMañanActionPerformed(evt);
+            }
+        });
+
+        RdBtTarde.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        RdBtTarde.setText("Tarde");
+
+        BttRegis.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        BttRegis.setText("Registrar participante");
+        BttRegis.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BttRegisActionPerformed(evt);
+            }
+        });
+
+        BttMosReg.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        BttMosReg.setText("Mostrar registros");
+        BttMosReg.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BttMosRegActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(178, 178, 178)
+                        .addComponent(jLbName, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(ChBoxAlm)
+                        .addGap(45, 45, 45)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(CBxElegir, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(ChBoxTrans)
+                                .addGap(38, 38, 38)
+                                .addComponent(ChBoxApo))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(88, 88, 88)
+                                .addComponent(RdBtMañan)
+                                .addGap(82, 82, 82))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(BttRegis)
+                                .addGap(32, 32, 32)))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(RdBtTarde)
+                            .addComponent(BttMosReg))))
+                .addContainerGap(38, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(24, 24, 24)
+                .addComponent(jLbName)
+                .addGap(18, 18, 18)
+                .addComponent(CBxElegir, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(31, 31, 31)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(ChBoxAlm)
+                    .addComponent(ChBoxTrans)
+                    .addComponent(ChBoxApo))
+                .addGap(33, 33, 33)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(RdBtMañan)
+                    .addComponent(RdBtTarde))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 42, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(BttRegis)
+                    .addComponent(BttMosReg))
+                .addGap(24, 24, 24))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void BttRegisActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BttRegisActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BttRegisActionPerformed
+
+    private void BttMosRegActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BttMosRegActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BttMosRegActionPerformed
+
+    private void RdBtMañanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RdBtMañanActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_RdBtMañanActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +192,15 @@ public class Interfaz extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BttMosReg;
+    private javax.swing.JButton BttRegis;
+    private javax.swing.JComboBox<String> CBxElegir;
+    private javax.swing.JCheckBox ChBoxAlm;
+    private javax.swing.JCheckBox ChBoxApo;
+    private javax.swing.JCheckBox ChBoxTrans;
+    private javax.swing.JRadioButton RdBtMañan;
+    private javax.swing.JRadioButton RdBtTarde;
+    private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JLabel jLbName;
     // End of variables declaration//GEN-END:variables
 }
